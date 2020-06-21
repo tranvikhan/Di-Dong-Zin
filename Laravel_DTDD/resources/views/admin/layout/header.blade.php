1ec67717150175fe5b2bd87feb622d62b10af50e
tranvikhan <tranvikhan@gmail.com>
2020-06-21 19:47:42 +0700

    Xoa toan bo file cu

--- Laravel_DTDD/resources/views/admin/layout/header.blade.php
+++
@@ -1,7 +0,0 @@
-<!-- TOP HEADER ..................................................................................-->
-<div class="top_header">
-    <img id="btn-menu" src="DiDongZin/assets/img/menu_100px.png" onclick="show_left_menu()">
-    <h2>DIDONGZIN MANAGEMENT CENTER</h2>
-    <a id="btn-admin">Chào! Admin</a>
-    <img id="admin-avt" src="DiDongZin/assets/img/cat_profile_30px.png">
-</div>
